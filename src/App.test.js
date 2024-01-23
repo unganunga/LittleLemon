d588c0a71772e292b9157067609cfea6cd92ffd2
unganunga <dunbaragain@gmail.com>
2024-01-23 19:50:41 +1100

    updated unit tests for form components

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import Bookings from './components/mainComponents/bookings';
-import { initializeTimes, updateTimes } from './components/mainComponents/bookingTimeFunctions';
+import useFormAPI from './components/mainComponents/bookingTimeFunctions';
+
+const { updateTimes, initializeTimes, submitForm} = useFormAPI()
 
 test('Renders the BookingForm heading', () => {
     const date =''
     const time = '17:00'
     const guests = 1
     const occasion = 'BirthDay'
-    const bookingStates = {date: date, time, guests,  occasion};
+    const availableTimes = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']
+    const bookingStates = {date: date, time, guests, occasion, availableTimes};
 
     render(<Bookings bookingStates={{...bookingStates}}/>);
     const headingElement = screen.getByText("Make a Reservation");
@@ -15,9 +18,12 @@ test('Renders the BookingForm heading', () => {
 })
 
 test('Test the updateTimes function', () => {
-  expect(updateTimes()).toEqual(['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'])
+  const times = updateTimes(null, '01/23/2024');
+  expect(times.sort()).toEqual(['15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:30', '19:30', '22:00', '23:00', '--- Select a Time ---'].sort())
 })
 
 test('Test the initializeTimes function', () => {
-  expect(initializeTimes()).toEqual(['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'])
+  const times = initializeTimes();
+  const numberOfTimes = times.length;
+  expect(numberOfTimes).toBeGreaterThan(0)
 })
