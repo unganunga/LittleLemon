1e086a795cf9135549a269192fedfc1490c9b2f6
unganunga <dunbaragain@gmail.com>
2024-03-13 16:18:50 +1100

    separated specials cards list into it's own component

--- src/components/mainComponents/specials.js
+++ src/components/mainComponents/specials.js
@@ -1,9 +1,6 @@
 import { Link } from "react-router-dom";
-import Card from "./card";
-import Salad from '../../images/greek salad.jpg'
 import Delivery from '../../images/delivery.png'
-import Bruchetta from '../../images/bruchetta.svg'
-import Cake from '../../images/lemon dessert.jpg'
+import SpecialCards from "./specialCards";
 
 function Specials() {
     return(
@@ -14,42 +11,7 @@ function Specials() {
                 <button>Menu</button>
                 </Link>
             </div>
-            <div className="specialsContainer">
-                <Card>
-                    <div className="cardImgContainer"><img src={Salad} height={'400px'}/></div>
-                    <div className="titlePrice">
-                        <h1 className="cardH1">Greek salad</h1>
-                        <p className="cardP">$12.99</p>
-                    </div>
-                    <p className="cardP">The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
-                    <div className="delivery">
-                        <Link to={'/order'} id='specialOrder'>Order online <img src={Delivery} height={'30px'}></img></Link>
-                    </div>
-                </Card>
-                <Card>
-                    <div className="cardImgContainer"><img src={Bruchetta} height={'400px'}/></div>
-                        <div className="titlePrice">
-                            <h1 className="cardH1">Bruchetta</h1>
-                            <p className="cardP">$5.99</p>
-                        </div>
-                        <p className="cardP">Our Bruchetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</p>
-                        <div className="delivery">
-                            <Link to={'/order'} id='specialOrder'>Order online <img src={Delivery} height={'30px'}></img></Link>
-                        </div>
-                </Card>
-                <Card>
-                    <div className="cardImgContainer"><img src={Cake} height={'400px'}/></div>
-                        <div className="titlePrice">
-                            <h1 className="cardH1">Lemon Dessert</h1>
-                            <p className="cardP">$5.00</p>
-                        </div>
-                        <p className="cardP">This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
-                        <div className="delivery">
-                            <Link to={'/order'} id='specialOrder'>Order online <img src={Delivery} height={'30px'}></img></Link>
-                        </div>
-                </Card>
-
-            </div>
+            <SpecialCards><Link to={'/order'} id='specialOrder'>Order online <img src={Delivery} height={'30px'}></img></Link></SpecialCards>
         </>
     )
 }
